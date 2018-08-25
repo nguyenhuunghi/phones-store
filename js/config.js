887325fcc79e0b51e5b1bde6cbca986eb5ecf1e8
nghi <nguyenhuunghiceo@gmail.com>
2018-08-25 13:09:57 +0700

    update config

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var host_heroku = 'https://phones-store.herokuapp.com/';
 var host_local = '#!'
-var html_url = host_local;
+var html_url = host_heroku;
 
 var phones_store_api = "https://phones-store-api.herokuapp.com/";
 var local_api = "http://127.0.0.1:5000/";
